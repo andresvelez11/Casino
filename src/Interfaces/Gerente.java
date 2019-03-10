673817e9b546229dc1db980e7e72ba5bf96a024f
andresvelez11 <andresvelez11@gmail.com>
2019-03-10 12:56:44 -0500

    Add files via upload

---
+++ src/Interfaces/Gerente.java
@@ -0,0 +1,43 @@
+package Interfaces;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Gerente {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gerente window = new Gerente();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Gerente() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1280, 720);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ src/Interfaces/Principal.java
@@ -0,0 +1,59 @@
+package Interfaces;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Principal {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal window = new Principal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Principal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100,1280, 720);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnGerente = new JButton("Gerente");
+		btnGerente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Gerente a=new Gerente();
+			}
+		});
+		btnGerente.setBounds(311, 450, 171, 41);
+		frame.getContentPane().add(btnGerente);
+		
+		JButton btnJugar = new JButton("Jugar");
+		btnJugar.setBounds(732, 450, 171, 41);
+		frame.getContentPane().add(btnJugar);
+	}
+}
